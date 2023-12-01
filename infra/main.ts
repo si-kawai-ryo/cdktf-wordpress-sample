5e9e19695a1ce7fae9a0c1503884cbd321589e8b
Ryoh Kawai <kawairyoh@gmail.com>
2023-12-01 18:44:38 +0900

    fix: add vpc

--- infra/main.ts
+++ infra/main.ts
@@ -1,11 +1,22 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
 
+import { AwsProvider } from "./.gen/providers/aws/provider";
+import { Vpc } from "./.gen/providers/aws/vpc";
+
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // define resources here
+    // Provider definition
+    new AwsProvider(this, "AWS", {
+      region: "ap-northeast-1",
+    });
+
+    // VPC
+    new Vpc(this, "vpc", {
+      cidrBlock: "10.0.0.0/16",
+    });
   }
 }
 
